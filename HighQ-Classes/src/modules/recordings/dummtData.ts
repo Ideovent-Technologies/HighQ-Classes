98feb13c7a4169eb8b5276a3d1855e3516ac171b
Sachin Prajapati <166122249+Sachinprajapa@users.noreply.github.com>
2025-07-21 19:09:37 +0530

    Added dummy data and Stsrted RecordingCard UI layout

---
+++ HighQ-Classes/src/modules/recordings/dummtData.ts
@@ -0,0 +1,6 @@
+
+export const recordings = [
+  { subject: "Physics", url: "https://dummyvideo.com/phy", date: "2025-07-18" },
+  { subject: "Chemistry", url: "https://dummyvideo.com/chem", date: "2025-07-17" },
+  { subject: "Math", url: "https://dummyvideo.com/math", date: "2025-07-16" },
+];
