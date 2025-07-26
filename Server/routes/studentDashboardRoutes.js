5b3055b42f1af006fdf4a3d44d6735496cc87531
Avinash Sharma <Avinashsharma31384@gmail.com>
2025-07-26 15:55:23 +0530

    After finishing the Recording API testing and updating the docs

--- Server/routes/studentDashboardRoutes.js
+++ Server/routes/studentDashboardRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { getStudentDashboard } from '../controllers/dashboard/studentDashboardController.js';
-import { protect, authorizeStudent } from '../middleware/authMiddleware.js';
+import { protect, authorize } from '../middleware/authMiddleware.js';
 
 
 const router = express.Router();
 
-router.get('/dashboard', protect, authorizeStudent, getStudentDashboard);
+router.get('/dashboard', protect, authorize('student'), getStudentDashboard);
 
 export default router;
