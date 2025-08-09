42ab3cff5e111975494414d8c3d67a109a692c86
ishikaaa18 <ishikasaha.trp@gmail.com>
2025-08-09 12:58:57 +0530

    setting page implemented

---
+++ HighQ-Classes/src/pages/dashboard/Settings.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Settings: React.FC = () => {
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h1>Settings</h1>
+            <p>Manage your account and application settings here.</p>
+            {/* Add your settings form or components below */}
+        </div>
+    );
+};
+
+export default Settings;
