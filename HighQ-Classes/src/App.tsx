620c76c17db30725b830a19a19ed8a599818a87b
Avinash Sharma <Avinashsharma31384@gmail.com>
2025-08-13 11:58:00 +0530

    THis commit before sumit pr merge b60faa6

--- HighQ-Classes/src/App.tsx
+++ HighQ-Classes/src/App.tsx
@@ -36,7 +36,6 @@ import Recordings from "@/components/dashboard/teacher/Recordings";
 import Batches from "@/components/dashboard/teacher/Batches";
 import Notices from "@/components/dashboard/teacher/Notices";
 import TeacherForm from "@/components/dashboard/teacher/TeacherForm";
-import BatchForm from "@/components/dashboard/batch/BatchForm";
 import TeacherManagementPage from "./pages/teacher/Teacher-Management";
 import CourseManagementPage from "./pages/course/Course-management";
 import CourseDetail from "@/components/dashboard/courses/CourseDetails";
@@ -467,15 +466,18 @@ const App: React.FC = () => {
                                     </ProtectedRoute>
                                 }
                             />
-                        <Route 
-    path="/dashboard/courses/add"
-    element={
-        <ProtectedRoute roles={["admin"]}>
-            <CourseForm />
-        </ProtectedRoute>
-    }
-/>
-<Route path="/dashboard/courses/:id/edit" element={<CourseForm />} />
+                            <Route
+                                path="/dashboard/courses/add"
+                                element={
+                                    <ProtectedRoute roles={["admin"]}>
+                                        <CourseForm />
+                                    </ProtectedRoute>
+                                }
+                            />
+                            <Route
+                                path="/dashboard/courses/:id/edit"
+                                element={<CourseForm />}
+                            />
                             <Route
                                 path="/dashboard/courses/manage"
                                 element={
